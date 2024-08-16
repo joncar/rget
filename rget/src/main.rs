0eefe4398442c9d518f1388883417860c4eb1f2d
Jon Caruana <jon@joncaruana.com>
2024-08-16 17:00:37 +0000

    Multiple requests.

--- rget/src/main.rs
+++ rget/src/main.rs
@@ -1,11 +1,19 @@
 use std::env;
 use tokio::fs::File;
+use tokio::io::AsyncSeekExt;
 use futures::StreamExt;
 use std::time::Instant;
+use http::StatusCode;
+use bytes::Bytes;
+use std::sync::mpsc::{Sender, Receiver, channel};
+use tokio::task::JoinSet;
 
-async fn fetch(url: &str, filename: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let mut f = File::create(filename).await?;
+struct WritePacket {
+    offset: u64,
+    buffer: Bytes
+}
 
+async fn get_content_length(url: &str) -> Result<u64, Box<dyn std::error::Error>> {
     let client = reqwest::Client::new();
     let response = client.head(url)
         .send()
@@ -17,41 +25,100 @@ async fn fetch(url: &str, filename: &str) -> Result<(), Box<dyn std::error::Erro
         .parse::<u64>()
         .unwrap();
 
-    dbg!(content_length);
+    Ok(content_length)
+}
 
-    let mut stream = client.get(url)
+async fn fetch_writes(filename: String, rx: Receiver<WritePacket>) -> Result<(), std::io::Error> {
+    let mut f = File::create(filename).await?;
+
+    while let Ok(packet) = rx.recv() {
+        f.seek(std::io::SeekFrom::Start(packet.offset)).await?;
+        tokio::io::copy(&mut packet.buffer.as_ref(), &mut f).await?;
+    }
+
+    Ok(())
+}
+
+async fn fetch_read(url: String, tx: Sender<WritePacket>, start_offset: u64, end_offset: u64) -> Result<(), reqwest::Error> {
+    let last_offset = end_offset - 1;
+
+    let client = reqwest::Client::new();
+
+    let response = client.get(url)
+        .header(reqwest::header::RANGE, format!("bytes={start_offset}-{last_offset}"))
         .send()
-        .await?
-        .bytes_stream();
+        .await?;
 
-    let mut offset = 0;
+    if response.status() != StatusCode::PARTIAL_CONTENT {
+        panic!("Server does not support Range header");
+    }
 
-    println!("Download started...");
-
-    let start_time = Instant::now();
+    let mut stream = response.bytes_stream();
+    let mut offset = start_offset;
 
     while let Some(item) = stream.next().await {
-        let buffer = item.unwrap();
-        let length = buffer.len();
+        let buffer: Bytes = item.unwrap();
+        let length = buffer.len() as u64;
 
-        //println!("Write to {offset}, {length} bytes");
-        tokio::io::copy(&mut buffer.as_ref(), &mut f).await?;
+        let packet = WritePacket { buffer, offset };
+        tx.send(packet).unwrap();
 
         offset += length;
     }
 
+    Ok(())
+}
+
+async fn fetch(url: &str, filename: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let start_time = Instant::now();
+
+    let content_length = get_content_length(url).await?;
+
+    let (tx, rx) = channel();
+
+    let fw = tokio::spawn(fetch_writes(String::from(filename), rx));
+
+    let max_request_length = 1024 * 1024;
+    let max_requests = 8;
+
+    let mut offset: u64 = 0;
+
+    let mut set = JoinSet::new();
+
+    loop {
+        if offset == content_length {
+            break
+        }
+
+        let end_offset = std::cmp::min(offset + max_request_length, content_length);
+
+        set.spawn(fetch_read(String::from(url), tx.clone(), offset, end_offset));
+
+        offset = end_offset;
+
+        if set.len() >= max_requests {
+            set.join_next().await.unwrap()??;
+        }
+    }
+
+    drop(tx);
+
+    while let Some(_) = set.join_next().await { }
+
+    fw.await.unwrap().unwrap();
+
     let elapsed = start_time.elapsed();
     println!("Duration: {elapsed:?}");
 
-    let Bps = (content_length as f64) / elapsed.as_secs_f64();
-    let MBps = Bps / (1024.0*1024.0);
-    let Mbps = MBps * 8.0;
-    println!("Rate: {Mbps} Mbps");
+    let bytes_per_sec = (content_length as f64) / elapsed.as_secs_f64();
+    let megabytes_per_sec = bytes_per_sec / (1024.0*1024.0);
+    let megabits_per_sec = megabytes_per_sec * 8.0;
+    println!("Rate: {megabits_per_sec} Mbps");
     
     Ok(())
 }
 
-#[tokio::main]
+#[tokio::main()]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
 
